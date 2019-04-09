eef7605e5a7365c1096b7d315124ab016a7da8ba
Don Syme <dsyme@microsoft.com>
2019-04-09 11:52:02 +0100

    fix 1

--- BuildErrorExample52/ThisFails.fs
+++ BuildErrorExample52/ThisFails.fs
@@ -3,4 +3,4 @@
 module ThisFails =
     // including the line bellow causes the following error:
     // FS0193	The module/namespace 'Control' from compilation unit 'FSharp.Core' did not contain the val 'ValLinkagePartialKey(OnSuccess)'
-    let f = ExampleAsyncLib62.AsyncFunctionsExample.f
+    let f = ExampleAsyncLib52.AsyncFunctionsExample.f
